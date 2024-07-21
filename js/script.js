47f74d072be3a72ece0b2e05129c44d223a0c999
gianlucafalaschi <gian100gian@outlook.it>
2024-07-21 13:23:59 +0200

    check if cards are matching

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 const cards = document.querySelectorAll('.card-square');
 
+let cardIsFlipped = false;
+let firstCard;
+let secondCard;
+
 //console.log(cards);
 
 for(let i = 0; i < cards.length; i++) {
@@ -13,6 +17,38 @@ for(let i = 0; i < cards.length; i++) {
 //Funzione che gira la carta
 function flipCard(){
     this.classList.toggle('clicked');
-    console.log('I was clicked');
-    console.log(this);
+    //console.log('I was clicked');
+    //console.log(this);
+
+    // al primo click (cardIsFlipped è false) 
+    if(!cardIsFlipped){
+        // cardIsFlipped diventa true e firstCard è la carta cliccata
+        cardIsFlipped = true;
+        firstCard = this;
+        /* console.log(cardIsFlipped);
+        console.log(firstCard);
+        console.log(firstCard.dataset.image); */
+    }
+    // al secondo click  cardIsFlipped è true, (la prima carta è stata cliccata)
+    else{ 
+        // cardIsFlipped diventa false
+        cardIsFlipped = false;
+        // la carta cliccata è secondCard
+        secondCard = this;
+        /* console.log(cardIsFlipped);
+        console.log(secondCard);
+        console.log(secondCard.dataset.image); */
+
+        // se le 2 carte sono uguali rimuovo l'addEventListener dalle carte
+        if(firstCard.dataset.image == secondCard.dataset.image) {
+            firstCard.removeEventListener('click', flipCard);
+            secondCard.removeEventListener('click', flipCard);
+        }else { // altrimenti se le 2 carte sono diverse rigiro entrambe le carte dopo 1 secondo
+            setTimeout(() => {
+                firstCard.classList.remove('clicked');
+                secondCard.classList.remove('clicked');
+              }, "1000");
+        }
+        
+    }
 } 
